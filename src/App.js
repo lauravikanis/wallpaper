2f74376d5c1fc36fcaacdf977224401901c918e2
lauravikanis <lauravikanis@yahoo.de>
2020-10-27 16:14:02 +0100

    add objects and base styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./global.css";
+import "./App.css";
+
+import ImagePreview from "./components/ImagePreview";
 
 function App() {
   return (
-    <>
-      <div className="App"></div>
-    </>
+    <main>
+      <button>Get Random Image</button>
+      <ImagePreview
+        src={
+          "https://images.unsplash.com/photo-1603550648164-519b522fdc1f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1652&q=80"
+        }
+        alt={"Image Text"}
+        author={"Flip"}
+      />
+    </main>
   );
 }
 
