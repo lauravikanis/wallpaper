2a72502714c49d56a0848c898a112e46c1924204
lauravikanis <lauravikanis@yahoo.de>
2020-10-28 17:13:48 +0100

    delete redundant div

--- src/components/FavoriteImage.js
+++ src/components/FavoriteImage.js
@@ -2,12 +2,10 @@ import "./FavoriteImage.css";
 
 export default function FavoriteImage({ photoId }) {
   return (
-    <div>
-      <img
-        className="favoriteImage"
-        src={`https://source.unsplash.com/${photoId}`}
-        alt="favourites"
-      />
-    </div>
+    <img
+      className="favoriteImage"
+      src={`https://source.unsplash.com/${photoId}`}
+      alt="favourites"
+    />
   );
 }
